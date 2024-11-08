09b01228aef63d6a5eab634b280af4f548d9f077
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-11-08 18:25:12 -0300

    init commit

---
+++ src/main/java/omcorp/ecowatt/EcowattApplication.java
@@ -0,0 +1,13 @@
+package omcorp.ecowatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcowattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcowattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/omcorp/ecowatt/EcowattApplicationTests.java
@@ -0,0 +1,13 @@
+package omcorp.ecowatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcowattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
